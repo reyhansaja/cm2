53818c3ea2de1a8c3e296f3ba353a2a6ea0e8f63
reyhansaja <raihan.ts16b@gmail.com>
2025-05-27 08:39:22 +0700

    fix kendaraan.java

---
+++ kendaraan.java
@@ -0,0 +1,11 @@
+public class kendaraan {
+    String platNomor;
+    String tipe;
+    String merk;
+
+    public kendaraan(String platNomor, String tipe, String merk){
+        this.platNomor = platNomor;
+        this.tipe = tipe;
+        this.merk = merk;
+    }
+}
